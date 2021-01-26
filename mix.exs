7041e241879a3b772c63ef20a91162edc45d888a
ontofractal <v@ontofractal.com>
2021-01-26 14:47:24 +0200

    init logflare API client

---
+++ mix.exs
@@ -0,0 +1,38 @@
+defmodule LogflareApiClient.MixProject do
+  use Mix.Project
+
+  def project do
+    [
+      app: :logflare_api_client,
+      version: "0.1.0",
+      elixir: "~> 1.11",
+      elixirc_paths: elixirc_paths(Mix.env()),
+      start_permanent: Mix.env() == :prod,
+      deps: deps()
+    ]
+  end
+
+  # Run "mix help compile.app" to learn about applications.
+  def application do
+    [
+      extra_applications: [:logger],
+      mod: {LogflareApiClient.Application, []}
+    ]
+  end
+
+  defp elixirc_paths(:test), do: ["lib", "test/support"]
+  defp elixirc_paths(_), do: ["lib"]
+
+  # Run "mix help deps" to learn about dependencies.
+  defp deps do
+    [
+      {:tesla, "~> 1.4.0"},
+      {:jason, ">= 1.0.0"},
+      {:mint, "~> 1.2.0"},
+      {:castore, "~> 0.1"},
+      {:bertex, "~> 1.3"},
+      {:bypass, "~> 2.1", only: :test},
+      {:mix_test_watch, "~> 1.0", only: :dev, runtime: false}
+    ]
+  end
+end
---
+++ test/logflare_api_client_test.exs
@@ -0,0 +1,59 @@
+defmodule LogflareApiClientTest do
+  use ExUnit.Case
+  alias LogflareApiClient
+  alias LogflareApiClient.TestUtils
+  require Logger
+
+  @port 4444
+  @path LogflareApiClient.api_path()
+
+  @api_key "l3kh47jsakf2370dasg"
+  @source_id "source2354551"
+
+  setup do
+    bypass = Bypass.open(port: @port)
+
+    {:ok, bypass: bypass}
+  end
+
+  test "ApiClient sends a correct POST request with gzip in bert format", %{bypass: bypass} do
+    response = Jason.encode!(%{"response" => "success"})
+    Bypass.expect_once(bypass, "POST", @path, fn conn ->
+      {:ok, body, conn} = Plug.Conn.read_body(conn)
+      assert {"x-api-key", @api_key} in conn.req_headers
+
+      body = TestUtils.decode_logger_body(body)
+
+      assert %{
+               "batch" => [
+                 %{
+                   "context" => %{"system" => %{"file" => "not_existing.ex"}},
+                   "level" => "info",
+                   "message" => "Logger message"
+                 }
+               ],
+               "source" => "source2354551"
+             } = body
+
+      Plug.Conn.resp(conn, 200, response)
+    end)
+
+    client = LogflareApiClient.new(%{api_key: @api_key, url: "http://localhost:#{@port}"})
+
+    batch = [
+      %{
+        "level" => "info",
+        "message" => "Logger message",
+        "context" => %{
+          "system" => %{
+            "file" => "not_existing.ex"
+          }
+        }
+      }
+    ]
+
+    {:ok, %{body: body}} = LogflareApiClient.post_logs(client, batch, @source_id)
+
+    assert body == response
+  end
+end
