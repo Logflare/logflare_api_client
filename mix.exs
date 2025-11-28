ec53d166df63b75e6471636e709f6f7056e874a6
TzeYiing <ty@tzeyiing.com>
2025-11-28 19:02:17 +0800

    chore: version bump

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule LogflareApiClient.MixProject do
   def project do
     [
       app: :logflare_api_client,
-      version: "0.3.5",
+      version: "0.3.6",
       elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
